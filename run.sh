d007afde7152185b3bd1b2b67769f0db7f3832bc
luiggyhm <154951314+luiggyhm@users.noreply.github.com>
2025-01-03 17:44:30 +0100

    Update run.sh
    to manually configure the db

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # mysql configuration
-cat << EOM > /etc/pure-ftpd/db/mysql.conf
+#cat << EOM > /etc/pure-ftpd/db/mysql.conf
 #MYSQLSocket        /var/run/mysqld/mysqld.sock
-MYSQLServer         ${MYSQL_HOST:-mysql}
-MYSQLPort           ${MYSQL_PORT:-3306}
-MYSQLUser           ${MYSQL_USER:-pureftpd}
-MYSQLPassword       ${MYSQL_PASSWORD:-password}
-MYSQLDatabase       ${MYSQL_DATABASE:-pureftpd}
-MYSQLCrypt          md5
-MYSQLGetPW          SELECT Password FROM ftpd WHERE User="\L" AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
-MYSQLGetUID         SELECT Uid FROM ftpd WHERE User="\L" AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
-MYSQLGetGID         SELECT Gid FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
-MYSQLGetDir         SELECT Dir FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
-MySQLGetBandwidthUL SELECT ULBandwidth FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
-MySQLGetBandwidthDL SELECT DLBandwidth FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
-MySQLGetQTASZ       SELECT QuotaSize FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
-MySQLGetQTAFS       SELECT QuotaFiles FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
-EOM
+#MYSQLServer         ${MYSQL_HOST:-mysql}
+#MYSQLPort           ${MYSQL_PORT:-3306}
+#MYSQLUser           ${MYSQL_USER:-pureftpd}
+#MYSQLPassword       ${MYSQL_PASSWORD:-password}
+#MYSQLDatabase       ${MYSQL_DATABASE:-pureftpd}
+#MYSQLCrypt          md5
+#MYSQLGetPW          SELECT Password FROM ftpd WHERE User="\L" AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
+#MYSQLGetUID         SELECT Uid FROM ftpd WHERE User="\L" AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
+#MYSQLGetGID         SELECT Gid FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
+#MYSQLGetDir         SELECT Dir FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
+#MySQLGetBandwidthUL SELECT ULBandwidth FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
+#MySQLGetBandwidthDL SELECT DLBandwidth FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
+#MySQLGetQTASZ       SELECT QuotaSize FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
+#MySQLGetQTAFS       SELECT QuotaFiles FROM ftpd WHERE User="\L"AND status="1" AND (ipaccess = "*" OR ipaccess LIKE "\R")
+#EOM
 
 # tls enable and certificate mix
 TLS=0
